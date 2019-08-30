a73a31576fb51f52850708a296e7d8dda2816973
fiqri khoirul m <fiqrikm18@gmail.com>
2019-08-30 14:04:36 +0700

    todolist api

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+use App\User;
+use DB;
+
+class TaskController extends Controller
+{
+    private $responseOk = 200;
+    private $responseBad = 502;
+
+    public function index(){
+        $taskCount = Task::count();
+        $task = Task::all();
+
+        if($taskCount != 0){
+            return $task;
+        }else{
+            return response()->json($this->responseBad);
+        }
+    }
+
+    public function getTaskByUser($id){
+        $data = DB::table("tasks")
+        ->select('tasks.task', 'tasks.created_at')
+        ->join("users", "users.id", "=", "tasks.user_id")
+        ->where(["users.id"=>$id])
+        ->get();
+
+        return $data;
+    }
+
+    public function createTask(Request $request){
+        $task = new Task;
+        $task->task = $request->task;
+        $task->user_id = $request->user_id;
+        $task->status = 'process';
+
+        if($task->save()){
+            return response()->json($this->responseOk);
+        }else{
+            return \response()->json($this->responseBad);
+        }
+    }
+
+    public function deleteTask($id){
+        $task = Task::find($id);
+        if($task->delete()){
+            return \response()->json($this->responseOk);
+        }else{
+            return response()->json($this->responseBad);
+        }
+    }
+}
